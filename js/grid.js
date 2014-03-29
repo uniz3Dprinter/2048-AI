e62942a4c7de558e99f2a416717825164f52827b
uniz3Dprinter <uniz3d@126.com>
2014-03-29 18:56:08 +0800

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -204,7 +204,7 @@ Grid.prototype.move = function (direction) {
           score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) {
+          if (merged.value === 20480000) {
             won = true;
           }
         } else {
